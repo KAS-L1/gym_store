453f1beaf79d763ee8249fdfc4dd43d1abc73378
KAS-L1 <kasl.54370906@gmail.com>
2024-11-11 22:34:47 +0800

    update

--- index.php
+++ index.php
@@ -8,18 +8,17 @@
 
 
 <?php
-    
-    if($PAGE != "index"){
-        
-        if(VIEW("page/", $PAGE) == "404"){
-            include("page/404.php");
-        }else{
-            include(VIEW("page/", $PAGE));    
-        }
-        
-    }else{
-        include('page/Dashboard.php');
+
+if ($PAGE != "index") {
+
+    if (VIEW("page/", $PAGE) == "404") {
+        include("page/404.php");
+    } else {
+        include(VIEW("page/", $PAGE));
     }
+} else {
+    include('page/Dashboard.php');
+}
 
 ?>
 
